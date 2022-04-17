06b66ad8537ab4273535601c32a939253a6d2109
victoryang00 <yangyiwei2000@gmail.com>
2022-04-18 07:43:38 +0800

    first commit

---
+++ src/test/java/test0.java
@@ -0,0 +1,10 @@
+public class test0 {
+    public static int main(String[] args) {
+        int a, b, c;
+        a = 0;
+        a = 1;
+        b = a * 2;
+        c = 3;
+        return c;
+    }
+}
